5919b6255537e91e35fe74717fe92973f22de2c0
KristinaBazyl <kristi40890@yandex.ru>
2024-04-23 20:33:47 +0300

    hw2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import rand.RandomDate;
+import rand.RandomDateProcessor;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        RandomDateProcessor.processRandomDate(myClass);
+        System.out.println(myClass.getDate());
+
+    }
+}
---
+++ src/main/java/org/example/MyClass.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import rand.RandomDate;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class MyClass {
+
+    @RandomDate(min = 1262304000000L, max =1293753600000L )
+    private Date date;
+    private Date date1;
+
+    private LocalDate localDate;
+    private LocalDateTime localDateTime;
+    private Instant instant;
+    public String getDate() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return simpleDateFormat.format(date);
+
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public Date getDate1() {
+        return date1;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+}

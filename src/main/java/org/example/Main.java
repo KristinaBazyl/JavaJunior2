f3e1f611d2e22902d7ba0e3708afe99bbeebb0aa
KristinaBazyl <kristi40890@yandex.ru>
2024-04-26 16:28:27 +0300

    hw3.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import rand.RandomDate;
 import rand.RandomDateProcessor;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(MysqlxDatatypes.Scalar.String[] args) {
         MyClass myClass = new MyClass();
         RandomDateProcessor.processRandomDate(myClass);
         System.out.println(myClass.getDate());
